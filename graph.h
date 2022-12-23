7087e9a8399aa002c469fe3be49ede7ac71921d8
Vortez Wohl <117743023+vortezwohl@users.noreply.github.com>
2022-12-24 03:40:50 +0800

    Update graph.h

--- graph.h
+++ graph.h
@@ -26,36 +26,36 @@ namespace wohl
 		vertex* v_remove(vertex* vert);								//remove all edges of a vertex
 		public:		
 		//command line print methods
-		void print_vertexes()										//which tells you the list of vertexes
+		void print_vertexes()									//which tells you the list of vertexes
 		{
 			printf("vertexes: ");
 			for(vertex* v=tail;v!=NULL;v=v->prior)
 			printf("v%d ",v->index);
 		}
-		void print_adjv(int idx,vertex* vert)						//print edges of vertex idx
+		void print_adjv(int idx,vertex* vert)							//print edges of vertex idx
 		{
 			printf("weight<v%d,v%d>=%d\n",idx,vert->index,vert->weight);
 		}
 		//con/destructors
 		vertex();
 		~vertex();
-		vertex(int idx,int weit);									//adjnext
-		vertex(int idx);											//next
+		vertex(int idx,int weit);								//adjnext
+		vertex(int idx);									//next
 		//methods of digraph
-		vertex* setv(int idx);										//create a new node
-		vertex* setv(int* idxs);									//create a series of nodes according to an array
-		vertex* sete(int idx1,int idx2,int weit);					//set an edge linking idx2 to idx1
-		vertex* rmv(int idx);										//remove an existing node
+		vertex* setv(int idx);									//create a new node
+		vertex* setv(int* idxs);								//create a series of nodes according to an array
+		vertex* sete(int idx1,int idx2,int weit);						//set an edge linking idx2 to idx1
+		vertex* rmv(int idx);									//remove an existing node
 		vertex* rme(int idx1,int idx2);								//remove an existing edge <idx1,idx2>
-		vertex** edgeof(int idx);									//return an array of address and weight
-		vertex* locate(int idx);									//return the address of current vertex in vertex list
+		vertex** edgeof(int idx);								//return an array of address and weight
+		vertex* locate(int idx);								//return the address of current vertex in vertex list
 		vertex* locate(int idx1,int idx2);							//return the address of current vertex in adjacency list of vertex idx1
 		void init_traversal_status();								//init_traversal_status
-		friend void traverse_around(vertex& graph,vertex* target);	//traverse around a vertex
-		friend void greedy_traversal(vertex& graph,vertex* target);	//greedily traverse the graph
-		friend void DFS(vertex& graph,vertex* target);				//depth first search
-		//friend void BFS_push_stack(vertex& graph,vertex* target);	//prepare the stack for BFS
-		//friend void BFS(vertex& graph,vertex* target);				//breadth first search
+		friend void traverse_around(vertex& graph,vertex* target);				//traverse around a vertex
+		friend void greedy_traversal(vertex& graph,vertex* target);				//greedily traverse the graph
+		friend void DFS(vertex& graph,vertex* target);						//depth first search
+		//friend void BFS_push_stack(vertex& graph,vertex* target);				//prepare the stack for BFS
+		//friend void BFS(vertex& graph,vertex* target);					//breadth first search
 		//more for developing...
 	};
 	class Graph:public vertex{};
